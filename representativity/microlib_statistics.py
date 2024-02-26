754410c73477e2a26a22e803e6c5045c13d3e752
amirDahari1 <amir.dahari@mail.huji.ac.il>
2024-02-26 17:18:12 +0000

    changed the structure of the json statistics

--- representativity/microlib_statistics.py
+++ representativity/microlib_statistics.py
@@ -17,64 +17,74 @@ The statistics generated are then saved in a json file, to be analyzed and used 
 representativity prediction of a new microstructure or micrograph.
 '''
 
-def insert_v_names_in_run_data(run_data, mode, n, v_names):
+def insert_v_names_in_all_data(all_data, mode, n, v_names, run_v_names, run_number=0):
     '''
     This function is used to insert the names of the variables that will be used to store the statistics'''
-    if n not in run_data[f'data_gen_{mode}']:
-            run_data[f'data_gen_{mode}'][n] = {}
-            for v_name in v_names:
-                if v_name not in run_data[f'data_gen_{mode}'][n]:
-                    run_data[f'data_gen_{mode}'][n][v_name] = {}
+    dim_data = all_data[f'data_gen_{mode}']
+    if n not in dim_data:
+        dim_data[n] = {}
+        for v_name in v_names:
+            if v_name not in dim_data[n]:
+                dim_data[n][v_name] = {}
+    micro_data = dim_data[n]
+    if f'run_{run_number}' not in micro_data:
+        micro_data[f'run_{run_number}'] = {}
+        run_data = micro_data[f'run_{run_number}']
+        for run_v_name in run_v_names:
+            if run_v_name not in run_data:
+                run_data[run_v_name] = {}
 
-def run_ir_prediction(netG, n, mode, imsize, run_data, conf):
+
+def run_ir_prediction(netG, n, mode, imsize, all_data, conf, run_number=0):
     '''
-    This function is used to predict the integral range of the microstructure.'''
+    This function is used to predict the integral range of the microstructure.
+    '''
+    run_data = all_data[f'data_gen_{mode}'][n][f'run_{run_number}']
     lf = imsize//32 + 2  # the size of G's input
     single_img = util.generate_image(netG, lf=lf, threed=mode=='3D', reps=1)
     if single_img.any():
         single_img = single_img.cpu()[0]
-        pred_err_vf, _, pred_ir_vf = util.make_error_prediction(single_img, conf=conf, model_error=False, correction=False, mxtpc=100)
+        pred_err_vf, _, pred_ir_vf = util.make_error_prediction(single_img, 
+                    conf=conf, model_error=False, correction=False, mxtpc=100)
         print(f'pred ir {imsize} = {np.round(pred_ir_vf, 3)}')
-        run_data[f'data_gen_{mode}'][n]['pred_ir_vf'][str(imsize)] = pred_ir_vf
-        run_data[f'data_gen_{mode}'][n]['pred_err_vf'][str(imsize)] = pred_err_vf
+        run_data['pred_ir_vf'][str(imsize)] = pred_ir_vf
+        run_data['pred_err_vf'][str(imsize)] = pred_err_vf
 
-def run_statistical_fit_analysis(netG, n, mode, edge_lengths_fit, run_data):
-    
+def run_statistical_fit_analysis(netG, n, mode, edge_lengths_fit, all_data):
+    '''
+    This function is used to run the statistical fit analysis on the microstructure, 
+    and find the "true" integral range of the microstructure.
+    '''
     imsize = 448 if mode=='3D' else 1600
     lf = imsize//32 + 2  # the size of G's input
     many_imgs = util.generate_image(netG, lf=lf, threed=mode=='3D', reps=50)
     vf = torch.mean(many_imgs).cpu().item()
     print(f'{n} vf = {vf}')
-    run_data[f'data_gen_{mode}'][n]['vf'] = vf
+    all_data[f'data_gen_{mode}'][n]['vf'] = vf
     fit_ir_vf = util.stat_analysis_error(many_imgs, vf, edge_lengths_fit)
-    run_data[f'data_gen_{mode}'][n]['fit_ir_vf'] = fit_ir_vf
+    all_data[f'data_gen_{mode}'][n]['fit_ir_vf'] = fit_ir_vf
     return fit_ir_vf, vf
 
 
-def compare_to_prediction(n, mode, edge_lengths_pred, fit_ir_vf, vf, run_data, conf):
-
+def compare_statistical_fit_error(n, mode, edge_lengths_pred, fit_ir_vf, vf, all_data, conf):
+    '''
+    This function is used to compare the statistical fit error to the prediction error.
+    '''
     n_dims = 2 if mode=='2D' else 3
     img_sizes = [(l,)*n_dims for l in edge_lengths_pred]
     fit_errs_vf = util.bernouli(vf, util.ns_from_dims(img_sizes, fit_ir_vf), conf=conf)
     for i in range(len(edge_lengths_pred)):
         imsize = edge_lengths_pred[i]
-        run_data[f'data_gen_{mode}'][n]['fit_err_vf'][imsize] = fit_errs_vf[i]
-    pred_errs = [run_data[f'data_gen_{mode}'][n]['pred_err_vf'][str(imsize)] for imsize in edge_lengths_pred]
-    err_dict = {edge_lengths_pred[i]: (fit_errs_vf[i], pred_errs[i]) for i in range(len(edge_lengths_pred))}
-    print(f'errors = {err_dict}')
+        all_data[f'data_gen_{mode}'][n]['fit_err_vf'][imsize] = fit_errs_vf[i]
 
-def json_preprocessing(run_number=0):
+def json_preprocessing():
     '''
     This function is used to load the data from the microlib dataset, and to prepare the json file
     '''
 
     # Load the statistics file
-    with open("microlib_statistics.json", "r") as fp:
+    with open("microlib_statistics_final.json", "r") as fp:
         all_data = json.load(fp)
-
-    if f'run_{run_number}' not in all_data:
-        all_data[f'run_{run_number}'] = {}
-    run_data = all_data[f'run_{run_number}']
 
     # Dataset path and list of subfolders
     with open("micro_names.json", "r") as fp:
@@ -83,31 +93,35 @@ def json_preprocessing(run_number=0):
     # Load placeholder generator
     netG = util.load_generator(micros[0])
 
-    v_names = ['vf', 'pred_ir_vf', 'pred_err_vf', 'fit_err_vf']
+    v_names = ['vf', 'fit_err_vf']
+    run_v_names = ['pred_ir_vf', 'pred_err_vf']
 
     modes = ['2D', '3D']
     for mode in modes:
-        if f'data_gen_{mode}' not in run_data:
-            run_data[f'data_gen_{mode}'] = {}
-
-    cur_modes = ['3D']  # change to ['2D'] or ['3D'] to only do one mode
-    run_prediction, run_statistical_fit = True, False
+        if f'data_gen_{mode}' not in all_data:
+            all_data[f'data_gen_{mode}'] = {}
 
     # Edge lengths for the experimental statistical analysis:
-    run_data['data_gen_2D']['edge_lengths_fit'] = list(range(500, 1000, 20))
-    run_data['data_gen_3D']['edge_lengths_fit'] = list(range(120, 400, 20))
+    all_data['data_gen_2D']['edge_lengths_fit'] = list(range(500, 1000, 20))
+    all_data['data_gen_3D']['edge_lengths_fit'] = list(range(120, 400, 20))
 
     # Edge lengths for the predicted integral range:
-    run_data['data_gen_2D']['edge_lengths_pred'] = list(range(8*32, 65*32, 4*32))
-    run_data['data_gen_3D']['edge_lengths_pred'] = list(range(4*32, 19*32, 1*32))
-    return all_data, run_data, micros, netG, cur_modes, v_names, run_prediction, run_statistical_fit
+    all_data['data_gen_2D']['edge_lengths_pred'] = list(range(8*32, 65*32, 4*32))
+    all_data['data_gen_3D']['edge_lengths_pred'] = list(range(4*32, 19*32, 1*32))
 
-def run_microlib_statistics(run_number=0):
+    return all_data, micros, netG, v_names, run_v_names
+
+def run_microlib_statistics(cur_modes=['2D', '3D'], run_s=False, run_p=True, run_number=0):
     '''
     This function is used to run the statistical analysis on the microlib dataset. 
-    It will generate the statistics for each microstructure in the dataset, and save it in a json file.'''
+    It will generate the statistics for each microstructure in the dataset, and save it in a json file.
+    param cur_modes: list of modes to run the statistical analysis on.
+    param run_s: if True, run the statistical fit analysis.
+    param run_p: if True, run the prediction of the integral range.
+    param run_number: the number of the run for the integral range prediction.
+    '''
 
-    all_data, run_data, micros, netG, cur_modes, v_names, run_p, run_s = json_preprocessing(run_number)
+    all_data, micros, netG, v_names, run_v_names = json_preprocessing()
 
     total_time_0 = time.time()
     # run the statistical analysis on the microlib dataset
@@ -123,34 +137,42 @@ def run_microlib_statistics(run_number=0):
 
             print(f'{mode} mode')
 
-            edge_lengths_fit = run_data[f'data_gen_{mode}']['edge_lengths_fit']
-            edge_lengths_pred = run_data[f'data_gen_{mode}']['edge_lengths_pred']
+            edge_lengths_fit = all_data[f'data_gen_{mode}']['edge_lengths_fit']
+            edge_lengths_pred = all_data[f'data_gen_{mode}']['edge_lengths_pred']
             
             n = p.split('/')[-1]
-            insert_v_names_in_run_data(run_data, mode, n, v_names)  # insert var names in run_data
+            insert_v_names_in_all_data(all_data, mode, n, v_names, run_v_names, run_number)  # insert var names in all_data
             
-            conf = 0.95  # confidence level for the prediction error
+            conf = 0.95  # confidence level for the prediction and stat. fit error 
 
             if run_p:
                 print(f'{n} starting prediction')
                 for imsize in edge_lengths_pred:
-                    run_ir_prediction(netG, n, mode, imsize, run_data, conf)
+                    run_ir_prediction(netG, n, mode, imsize, all_data, conf, run_number)
 
             if run_s:
                 print(f'{n} starting statistical fit')
-                fit_ir_vf, vf = run_statistical_fit_analysis(netG, n, mode, edge_lengths_fit, run_data)
-                compare_to_prediction(n, mode, edge_lengths_pred, fit_ir_vf, vf, run_data, conf)
+                fit_ir_vf, vf = run_statistical_fit_analysis(netG, n, mode, edge_lengths_fit, all_data)
+                compare_statistical_fit_error(n, mode, edge_lengths_pred, fit_ir_vf, vf, all_data, conf)
             
-            print()
-            with open(f"microlib_statistics.json", "w") as fp:
+            with open(f"microlib_statistics_final.json", "w") as fp:
                 json.dump(all_data, fp) 
 
-        print(f'time for one micro {cur_modes} = {np.round((time.time()-t_micro)/60, 2)} minutes')
+        print(f'\ntime for micro {n} {cur_modes} = {np.round((time.time()-t_micro)/60, 2)} minutes\n')
 
         print(f'{j+1}/{len(micros)} microstructures done')
     print(f'total time = {np.round((time.time()-total_time_0)/60, 2)} minutes')
 
+def main_run_microlib_statistics(cur_modes=['2D', '3D'], num_runs=5):
+    # first run the statistical analysis on the microlib dataset
+    print(f'Running statistical analysis fit on {cur_modes} mode(s)')
+    run_microlib_statistics(cur_modes=cur_modes, run_s=True, run_p=False)
+    # then run the integral range prediction multiple times
+    print(f'Running integral range prediction on {cur_modes} mode(s)')
+    for run_number in range(num_runs):
+        print(f'Run number {run_number}\n')
+        run_microlib_statistics(cur_modes, run_s=False, run_p=True, run_number=run_number)
+
 
 if __name__ == '__main__':
-    for i in range(5):
-        run_microlib_statistics(5)
+    main_run_microlib_statistics(cur_modes=['3D'], num_runs=5)
--- representativity/prediction_error.py
+++ representativity/prediction_error.py
@@ -11,12 +11,12 @@ Description: This script is used to analyse the prediction error of the integral
 of the microstructures from the microlib dataset. 
 '''
 
-def error_by_size_estimation(dim, run_idx=0):
-    data_dim, micro_names, slope_to_fit = data_micros_and_slope(dim, run_idx)
+def error_by_size_estimation(dim, run_number=0):
+    data_dim, micro_names, slope_to_fit = data_micros_and_slope(dim)
     edge_lengths_pred = data_dim['edge_lengths_pred']
     stds = []
     for edge_length in edge_lengths_pred:
-        ir_results, err_results = comparison_results(data_dim, micro_names, str(edge_length))
+        ir_results, err_results = comparison_results(data_dim, micro_names, str(edge_length), run_number)
 
         slope = slope_to_fit[dim]["Integral Range"]
         y_data = slope*ir_results[1] 
@@ -34,29 +34,27 @@ def error_by_size_estimation(dim, run_idx=0):
         print(f'error = {err}')
     return stds
 
-def data_micros_and_slope(dim, run_idx=0):
-    with open("microlib_statistics.json", "r") as fp:
+def data_micros_and_slope(dim):
+    with open("microlib_statistics_final.json", "r") as fp:
         datafull = json.load(fp)
 
     with open("micro_names.json", "r") as fp:
         micro_names = json.load(fp)
 
-    run_data = datafull[f'run_{run_idx}']
-    run_data = run_data[f'data_gen_{dim}']
-    
+    dim_data = datafull[f'data_gen_{dim}']
     
     # Slope for tpc to stat.analysis error fit, that have 0 mean: 
     slope_to_fit = {'2D': {'Integral Range': 1},
                     '3D': {'Integral Range': 0.95}}
     
-    return run_data, micro_names, slope_to_fit
+    return dim_data, micro_names, slope_to_fit
 
-def comparison_results(data_dim, micro_names, edge_length):
-    
-    fit_ir_vf = np.array([data_dim[n]['fit_ir_vf'] for n in micro_names])
-    fit_err_vf = np.array([data_dim[n]['fit_err_vf'][edge_length] for n in micro_names])
-    pred_ir_vf = np.array([data_dim[n]['pred_ir_vf'][edge_length] for n in micro_names])
-    pred_err_vf = np.array([data_dim[n]['pred_err_vf'][edge_length] for n in micro_names])
+def comparison_results(data_dim, micro_names, edge_length, run_number=0):
+    micros_data = [data_dim[n] for n in micro_names]
+    fit_ir_vf = np.array([m_data['fit_ir_vf'] for m_data in micros_data])
+    fit_err_vf = np.array([m_data['fit_err_vf'][edge_length] for m_data in micros_data])
+    pred_ir_vf = np.array([m_data[f'run_{run_number}']['pred_ir_vf'][edge_length] for m_data in micros_data])
+    pred_err_vf = np.array([m_data[f'run_{run_number}']['pred_err_vf'][edge_length] for m_data in micros_data])
     
     ir_results = [fit_ir_vf, pred_ir_vf]
     err_results = [fit_err_vf, pred_err_vf]
@@ -71,9 +69,10 @@ def plot_error_by_size(errs, edge_lengths_pred, dim):
 
 if __name__ == '__main__':
     stds = []
-    for i in range(5):
+    num_runs = 10
+    for i in range(num_runs):
         stds.append(error_by_size_estimation('2D', i))
-    stds = np.array(stds).sum(axis=0)/5
+    stds = np.array(stds).sum(axis=0)/num_runs
     run_data, _, _ = data_micros_and_slope('2D')
     edge_lengths_pred = run_data['edge_lengths_pred']
     plot_error_by_size(stds, edge_lengths_pred, '2D')
