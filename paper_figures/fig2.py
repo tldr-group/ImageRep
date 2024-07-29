80960489dbb06ce1cf1ac798fce8a96022c2c09f
amirDahari1 <amir.dahari@mail.huji.ac.il>
2024-07-29 18:56:26 +0100

    starting to re-do fig2

--- paper_figures/fig2.py
+++ paper_figures/fig2.py
@@ -7,97 +7,114 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy import stats
 import torch
-from representativity import util
+from representativity import util, microlib_statistics
 from torch.nn.functional import interpolate
 
-with open("data_gen2.json", "r") as fp:
-    data = json.load(fp)["generated_data"]
+# with open("data_gen2.json", "r") as fp:
+#     data = json.load(fp)["generated_data"]
 
-l=len(list(data.keys()))
+# l=len(list(data.keys()))
 # l=3
 c=[(0,0,0), (0.5,0.5,0.5)]
 # plotting = [f'microstructure{f}' for f in [235, 209,205,177]]
-plotting = [f'microstructure{f}' for f in [235, 228, 205,177]]
+plotting_ims = [f'microstructure{f}' for f in [235, 228, 205,177]]
 
 # plotting = [k for k in data.keys()]
-l = len(plotting)
+l = len(plotting_ims)
 fig, axs = plt.subplots(l, 3)
 fig.set_size_inches(12, l*4)
 preds = [[],[]]
 irs = [[],[]]
-sas =[]
-i=0
-for n in list(data.keys()):
-    if n not in plotting:
+
+all_data, micros, netG, v_names, run_v_names = microlib_statistics.json_preprocessing()
+lens_for_fit = list(range(500, 1000, 20))  # lengths of images for fitting L_characteristic
+plotting_ims = [micro for micro in micros if micro.split('/')[-1] in plotting_ims]
+# run the statistical analysis on the microlib dataset
+for i, p in enumerate(plotting_ims):
+
+    try:
+        netG.load_state_dict(torch.load(p + "_Gen.pt"))
+    except:  # if the image is greayscale it's excepting because there's only 1 channel
         continue
-    img = tifffile.imread(f'/home/amir/microlibDataset/{n}/{n}.tif')
-    d = data[n]
-    d1 = data[n]
-    
+    imsize = 1600
+    lf = imsize//32 + 2  # the size of G's input
+    many_images = util.generate_image(netG, lf=lf, threed=False, reps=50)
+    pf = many_images.mean().cpu().numpy()
+    small_imsize = 512
+    img = many_images[0].detach().cpu().numpy()[:small_imsize, :small_imsize]
+
     csets = [['black', 'black'], ['gray', 'gray']]
-    for j, met in enumerate(['vf', 'sa']):
-        cs = csets[j]
-        img_dims = [np.array([int(im_len)]*2) for im_len in d['ls']]
-        ns = util.ns_from_dims(img_dims, d[f'ir_{met}'])
-        berns_vf = util.bernouli(d[f'{met}'], ns)
-        axs[i, 1].scatter(d['ls'], d[f'err_exp_{met}'], c=cs[0], s=8, marker = 'x', label = f'{met} errors from sampling')
-        axs[i, 1].plot(d['ls'], berns_vf, c=cs[0], label = f'{met} errors from fitted IR')
-        # axs[i, 1].plot(d[f'ls'], d[f'err_model_{met}'], c=cs[0], label = f'{met} errors from bernouli') 
-        y = d[f'tpc_{met}'][0]  # TODO write that in sa tpc, only the first direction is shown, or do something else, maybe normalise the tpc? We can do sum, because that's how we calculate the ir!
-        x = d[f'tpc_{met}_dist']
-        y = np.array(y)
-        
-        # TODO erase this afterwards:
-        if met=='vf':
-            ir = np.round(d[f'ir_vf'], 1)
-            axs[i, 2].plot(x, y, c=cs[1], label=f'Volume fraction 2PC')
-            axs[i, 2].axhline(d['vf']**2, linestyle='dashed', label='$p^2$')
-            axs[i, 2].plot([0,ir],[d['vf']**2-0.02, d['vf']**2-0.02], c='green', linewidth=3, label=r'$\tilde{a}_2$')
-            ticks = [0, int(ir), 20, 40, 60, 80, 100]
-            ticklabels = map(str, ticks)
-            axs[i, 2].set_xticks(ticks)
-            axs[i, 2].set_xticklabels(ticklabels)
-            axs[i,2].fill_between(x, d['vf']**2, y, alpha=0.5, label=f'Integrated part')
-            axs[i,2].legend()
-            
-            
-        # axs[i, 2].scatter(x[knee], y_data[knee]/y.max(), c =cs[1], marker = 'x', label=f'{met} ir from tpc', s=100)
-        ir = d[f'ir_{met}']
-        pred_ir = util.tpc_to_ir(d[f'tpc_{met}_dist'], d[f'tpc_{met}']) 
-        pred_ir = pred_ir * 1.61
-        # axs[i, 2].scatter(x[round(pred_ir)], y[round(pred_ir)], c =cs[1], marker = 'x', label=f'{met} predicted tpc IR', s=100)
-        # axs[i, 2].scatter(x[round(ir)], y[round(ir)], facecolors='none', edgecolors = cs[1], label=f'{met} fitted IR', s=100)
-
-        irs[j].append(ir)
-        if i ==0:
-            axs[i,1].legend()
-            axs[i,2].legend()
-        axs[i,1].set_xlabel('Image length size [pixels]')
-        axs[i,1].set_ylabel('Volume fraction percentage error [%]')
-        axs[i,2].set_ylabel('2-point correlation function')
-    ir = np.round(d[f'ir_vf'], 2)
-    sas.append(d['sa'])
-    im = img[0]*255
-    si_size, nirs = 160, 5
-    sicrop = int(ir*nirs)
-    print(ir, sicrop)
-    subim=torch.tensor(im[-sicrop:,-sicrop:]).unsqueeze(0).unsqueeze(0).float()
-    subim = interpolate(subim, size=(si_size,si_size), mode='nearest')[0,0]
-    subim = np.stack([subim]*3, axis=-1)
+    conf = 0.95
+    errs = util.real_image_stats(many_images, lens_for_fit, pf, conf=conf)
+    stds = errs / stats.norm.interval(conf)[1] * pf / 100
+    # print(stds)
+    vars = stds**2
+    # from variations to L_characteristic using image size and phase fraction
+    clss = (np.array(lens_for_fit)**2*vars/pf/(1-pf))**0.5
+    print(clss)
     
-    subim[:5,:,:] = 125
-    subim[:,:5,:] = 125
-    # subim[5:20, 5:50, :]
-    subim[10:15, 10:10+si_size//nirs, :] = 0
-    subim[10:15, 10:10+si_size//nirs, 1:-1] = 125
+    # cs = csets[j]
+    # img_dims = [np.array([int(im_len)]*2) for im_len in d['ls']]
+    # ns = util.ns_from_dims(img_dims, d[f'ir_{met}'])
+    # berns_vf = util.bernouli(d[f'{met}'], ns)
+    # axs[i, 1].scatter(d['ls'], d[f'err_exp_{met}'], c=cs[0], s=8, marker = 'x', label = f'{met} errors from sampling')
+    axs[i, 1].plot(lens_for_fit, clss, label = f'Characteristic length scale')
+    axs[i, 1].set_ylim(0, 28)
+    # # axs[i, 1].plot(d[f'ls'], d[f'err_model_{met}'], c=cs[0], label = f'{met} errors from bernouli') 
+    # y = d[f'tpc_{met}'][0]  # TODO write that in sa tpc, only the first direction is shown, or do something else, maybe normalise the tpc? We can do sum, because that's how we calculate the ir!
+    # x = d[f'tpc_{met}_dist']
+    # y = np.array(y)
+    
+    # # TODO erase this afterwards:
+    # if met=='vf':
+    #     ir = np.round(d[f'ir_vf'], 1)
+    #     axs[i, 2].plot(x, y, c=cs[1], label=f'Volume fraction 2PC')
+    #     axs[i, 2].axhline(d['vf']**2, linestyle='dashed', label='$p^2$')
+    # axs[i, 2].plot([0,ir],[d['vf']**2-0.02, d['vf']**2-0.02], c='green', linewidth=3, label=r'$\tilde{a}_2$')
+    # ticks = [0, int(ir), 20, 40, 60, 80, 100]
+    # ticklabels = map(str, ticks)
+    # axs[i, 2].set_xticks(ticks)
+    # axs[i, 2].set_xticklabels(ticklabels)
+    # axs[i,2].fill_between(x, d['vf']**2, y, alpha=0.5, label=f'Integrated part')
+    # axs[i,2].legend()
+        
+        
+    # # axs[i, 2].scatter(x[knee], y_data[knee]/y.max(), c =cs[1], marker = 'x', label=f'{met} ir from tpc', s=100)
+    # ir = d[f'ir_{met}']
+    # pred_ir = util.tpc_to_ir(d[f'tpc_{met}_dist'], d[f'tpc_{met}']) 
+    # pred_ir = pred_ir * 1.61
+    # # axs[i, 2].scatter(x[round(pred_ir)], y[round(pred_ir)], c =cs[1], marker = 'x', label=f'{met} predicted tpc IR', s=100)
+    # # axs[i, 2].scatter(x[round(ir)], y[round(ir)], facecolors='none', edgecolors = cs[1], label=f'{met} fitted IR', s=100)
 
-    im = np.stack([im]*3, axis=-1)
-    im[-si_size:,-si_size:] = subim
-    axs[i, 0].imshow(im)
-    axs[i, 0].set_xticks([])
-    axs[i, 0].set_yticks([])
-    axs[i, 0].set_ylabel(f'M{n[1:]}')
-    axs[i, 0].set_xlabel(f'Volume fraction '+ r'$\tilde{a}_2$: '+ f'{ir}   Inset mag: x{np.round(si_size/sicrop, 2)}')
+    # irs[j].append(ir)
+    # if i ==0:
+    #     axs[i,1].legend()
+    #     axs[i,2].legend()
+    # axs[i,1].set_xlabel('Image length size [pixels]')
+    # axs[i,1].set_ylabel('Volume fraction percentage error [%]')
+    # axs[i,2].set_ylabel('2-point correlation function')
+    # ir = np.round(d[f'ir_vf'], 2)
+    # im = img[0]*255
+    # si_size, nirs = 160, 5
+    # sicrop = int(ir*nirs)
+    # print(ir, sicrop)
+    # subim=torch.tensor(im[-sicrop:,-sicrop:]).unsqueeze(0).unsqueeze(0).float()
+    # subim = interpolate(subim, size=(si_size,si_size), mode='nearest')[0,0]
+    # subim = np.stack([subim]*3, axis=-1)
+    
+    # subim[:5,:,:] = 125
+    # subim[:,:5,:] = 125
+    # # subim[5:20, 5:50, :]
+    # subim[10:15, 10:10+si_size//nirs, :] = 0
+    # subim[10:15, 10:10+si_size//nirs, 1:-1] = 125
+
+    # im = np.stack([im]*3, axis=-1)
+    # im[-si_size:,-si_size:] = subim
+    # axs[i, 0].imshow(im)
+    # axs[i, 0].set_xticks([])
+    # axs[i, 0].set_yticks([])
+    # axs[i, 0].set_ylabel(f'M{n[1:]}')
+    # axs[i, 0].set_xlabel(f'Volume fraction '+ r'$\tilde{a}_2$: '+ f'{ir}   Inset mag: x{np.round(si_size/sicrop, 2)}')
 
     i+=1
 
--- paper_figures/prediction interval.py
+++ paper_figures/prediction interval.py
@@ -12,8 +12,8 @@ def plot_likelihood_of_phi(image_pf, pred_std, std_dist_std):
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
     im = np.load('im_for_prediction_interval_fig.npy')
     ax[0,0].imshow(im[:200,:200], cmap='gray')
-    ax[0,0].set_xticks([0,150])
-    ax[0,0].set_yticks([0,150])
+    ax[0,0].set_xticks([])
+    ax[0,0].set_yticks([])
     ax[0,0].set_title('(a)')
     # ax[0,0].set_ylabel('Sample image')
     # ax[0,0].axis('off')
@@ -42,28 +42,34 @@ def plot_likelihood_of_phi(image_pf, pred_std, std_dist_std):
     # before normalising by weight:
     pf_dist_before_norm = util.normal_dist(pf_mesh, mean=pf_locs, std=std_mesh)
     mid_std_dist = pf_dist_before_norm[std_dist_divisions//2,:]
-    ax[0,2].plot(pf_x_1d, mid_std_dist, c='orange', label='Likelihood of true phase \nfraction, completely trusting\nstd prediction as true std')
+    ax[0,2].plot(pf_x_1d, mid_std_dist, c='orange', label='Likelihood of the material\nphase fraction, trusting\nstd prediction as true std')
     ax[0,2].vlines(image_pf, 0, np.max(mid_std_dist),  linestyle="--", color='g', label = 'Observed phase fraction $\Phi(\omega)$')
     ax[0,2].set_title('(c)')
     ax[0,2].set_xlabel('Phase fraction')
     ax[0,2].set_yticks([0,300])
     ax[0,2].legend()
 
-    ax[1,0].contourf(pf_mesh, std_mesh, pf_dist_before_norm, levels=100, cmap = 'plasma') 
+    c1 = ax[1,0].contourf(pf_mesh, std_mesh, pf_dist_before_norm, levels=100, cmap = 'plasma') 
+    for c in c1.collections:
+        c.set_edgecolor("face")
     # ax[1,0].vlines(image_pf, x_std_dist_bounds[0], x_std_dist_bounds[1], colors='g', label = 'Observed $\Phi(\omega)$')
-    ax[1,0].hlines(pred_std, pf_x_bounds[0], pf_x_bounds[1], colors='orange', label = "(c) - Likelihood of true phase\nfraction using predicted std")
+    ax[1,0].hlines(pred_std, pf_x_bounds[0], pf_x_bounds[1], colors='orange', label = "(c) - Likelihood of the material\nphase fraction using predicted\nstd")
     ax[1,0].set_title('(d)')
     ax[1,0].set_xlabel('Phase fraction')
     ax[1,0].set_ylabel('Standard deviation')
+    ax[1,0].set_yticks(ax[1,0].get_yticks()[1:-1:2])
     ax[1,0].legend()
     # normalise by weight:
     pf_dist = (pf_dist_before_norm.T * std_dist).T
-    contour = ax[1,1].contourf(pf_mesh, std_mesh, pf_dist, levels=100, cmap = 'plasma') 
+    c2 = ax[1,1].contourf(pf_mesh, std_mesh, pf_dist, levels=100, cmap = 'plasma') 
+    for c in c2.collections:
+        c.set_edgecolor("face")
     ax[1,1].set_title('(e)')
     ax[1,1].set_xlabel('Phase fraction')
     ax[1,1].set_ylabel('Standard deviation')
     ax[1,1].vlines(image_pf, x_std_dist_bounds[0], x_std_dist_bounds[1], colors='g', label = 'Multiplying each row by std\nlikelihood weights of (b)')
     ax[1,1].hlines(pred_std, pf_x_bounds[0], pf_x_bounds[1], colors='orange', label = '(c) after multiplied by the\ncenter weight of (b)')
+    ax[1,1].set_yticks(ax[1,1].get_yticks()[1:-1:2])
     ax[1,1].legend()
     pf_dist_integral_col = np.trapz(pf_dist, x_std_dist, axis=0)
     pf_dist_integral = np.trapz(pf_dist_integral_col, pf_x_1d)
@@ -94,7 +100,7 @@ def plot_likelihood_of_phi(image_pf, pred_std, std_dist_std):
     alpha_mid_std_dist_end = np.where(cum_sum_mid_std_dist > alpha)[0][0]
     alpha_mid_std_dist_beginning = np.where(cum_sum_mid_std_dist > 1-alpha)[0][0]
     
-    ax[1,2].plot(pf_x_1d, sum_dist_norm, c='blue', label = "Likelihood of true phase fraction\nusing integration of (e)'s rows")
+    ax[1,2].plot(pf_x_1d, sum_dist_norm, c='blue', label = "Likelihood of the material\nphase fraction by integrating\nthe rows of (e)")
     # new_std_dist = normal_dist(pf_x_1d, mean=image_pf, std=0.02775)
     # ax[1,2].plot(pf_x_1d, new_std_dist, c='green', label = 'new std dist')
     ax[1,2].vlines(pf_x_1d[alpha_sum_dist_norm_end], 0, np.max(sum_dist_norm), linestyle="--", color='blue', label = r'95% confidence bounds')
@@ -109,6 +115,7 @@ def plot_likelihood_of_phi(image_pf, pred_std, std_dist_std):
     ax[1,2].legend()
     print(np.trapz(sum_dist_norm, pf_x_1d))
     print(np.trapz(mid_std_dist, pf_x_1d))
+    plt.savefig('prediction_interval.pdf', format='pdf')
 
 
 
