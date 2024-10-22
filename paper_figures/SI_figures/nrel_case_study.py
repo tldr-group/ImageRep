4ebc1427cea7bff55cd9f33a30ae5ee1cb6e508a
amirDahari1 <amir.dahari@mail.huji.ac.il>
2024-10-22 17:49:21 +0100

    nrel example update

--- paper_figures/SI_figures/nrel_case_study.py
+++ paper_figures/SI_figures/nrel_case_study.py
@@ -2,9 +2,19 @@ import pybamm
 import matplotlib.pyplot as plt
 
 
-def update_phase_fraction(params, am_pf):
+def update_phase_fraction(params, porosity):
+    # cbd = 0.14
+    # am_pf = am_pf + cbd
+    am_pf = 1 - porosity
+    original_am_pf = params["Positive electrode active material volume fraction"]
+    original_am_pf += 0.07
     params["Positive electrode active material volume fraction"] = am_pf
-    params["Positive electrode porosity"] = 1 - am_pf
+    # params["Positive electrode thickness [m]"] *= (am_pf/original_am_pf)
+    # print(f"electrode thickness = {params['Positive electrode thickness [m]']}")
+    # params["Positive electrode density [kg.m-3]"] *= am_pf/original_am_pf
+    params["Maximum concentration in positive electrode [mol.m-3]"] *= am_pf/original_am_pf
+    
+    params["Positive electrode porosity"] = porosity
     params["Positive electrode Bruggeman coefficient (electrode)"] = 1.5
     return params
 
@@ -12,7 +22,10 @@ def run_simulation(params):
     model = pybamm.lithium_ion.DFN()
     sim = pybamm.Simulation(model, parameter_values=params)
     current = params["Current function [A]"]
-    sim.solve([0, 15000/current])
+    if current < 1:
+        sim.solve([0, 18500/current])
+    else:
+        sim.solve([0, 593])
     return sim
 
 def plot_results(ax, sim, phase_fraction, param_values):
@@ -20,13 +33,15 @@ def plot_results(ax, sim, phase_fraction, param_values):
     voltage = sim.solution["Terminal voltage [V]"]
     print(f"Last Ah = {discharge_capacity.entries[-1]}")
     current = param_values["Current function [A]"]
-    ax.plot(discharge_capacity.entries, voltage.entries, label=f"phase fraction = {phase_fraction} (current = {current} A)")
+    ax.plot(discharge_capacity.entries, voltage.entries, label=f"Porosity = {phase_fraction} (current = {current} A)")
     ax.set_xlabel("Discharge Capacity / Ah")
     ax.set_ylabel("Voltage / V")
 
-def run_and_plot(ax, param_values, phase_fraction1, phase_fraction2, amps):
-    param_values["Current function [A]"] = amps
+def run_and_plot(ax, phase_fraction1, phase_fraction2, amps):
     for phase_fraction in [phase_fraction1, phase_fraction2]:
+        # Reset param values
+        param_values = pybamm.ParameterValues("Chen2020")
+        param_values["Current function [A]"] = amps
         param_values = update_phase_fraction(param_values, phase_fraction)
         simulation = run_simulation(param_values)
         plot_results(ax, simulation, phase_fraction, param_values)
@@ -35,11 +50,10 @@ def run_and_plot(ax, param_values, phase_fraction1, phase_fraction2, amps):
 
 if __name__ == "__main__":
     # Load parameter values
-    param_values = pybamm.ParameterValues("Chen2020")
     
     # Update parameter values
-    phase_fraction1 = 0.397
-    phase_fraction2 = 0.429
+    phase_fraction1 = 0.351
+    phase_fraction2 = 0.380
     
     # Create a 2x2 figure, showing on the right side the discharge curves and on the left
     # side the two screenshots taken from www.imagerep.io for the nrel dataset:
@@ -60,14 +74,18 @@ if __name__ == "__main__":
 
     # Discharge curve for phase fraction 1
     ax_discharge1 = axs[0, 1]
-    amps = 1
-    run_and_plot(ax_discharge1, param_values, phase_fraction1, phase_fraction2, amps)
+    amps = 5.09/100
+    c_rate = "C/100"
+    run_and_plot(ax_discharge1, phase_fraction1, phase_fraction2, amps)
     ax_discharge1.set_title("(b)")
+
+    
 
     # Discharge curve for phase fraction 2
     ax_discharge2 = axs[1, 1]
-    amps = 14
-    run_and_plot(ax_discharge2, param_values, phase_fraction1, phase_fraction2, amps)
+    amps = 5.09*3
+    c_rate = "3C"
+    run_and_plot(ax_discharge2, phase_fraction1, phase_fraction2, amps)
 
     plt.tight_layout()
     plt.subplots_adjust(wspace=0.5)
